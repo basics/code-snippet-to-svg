7f136b0eae38b901575bc4085828bd82d16de770
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-08-23 10:52:21 +0200

    fix(optimize): changed parameter transaction

--- src/index.js
+++ src/index.js
@@ -16,10 +16,16 @@ function highlightCode(code, lang = '') {
   return emphasize.highlightAuto(code);
 }
 
-function codeToSVG(code, min, max, lang, theme = '3024.night') {
+function codeToSVG(code, min, max, { lang, theme = '3024.night' }) {
   const extractedCode = extract(code, min, max);
   const highlightedCode = highlightCode(extractedCode, lang);
   return ansiToSVG(highlightedCode.value, {
+    fontSize: 16,
+    lineHeight: 25.6,
+    paddingTop: 10,
+    paddingLeft: 10,
+    paddingBottom: 10,
+    paddingRight: 10,
     colors: path.resolve(__dirname, `../schemes/${theme}.itermcolors`)
   });
 }
