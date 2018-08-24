26af0a51a63c7d6a2a35f919c256cd3466c14e22
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-08-24 19:13:20 +0200

    fix(eslint): auto fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const ansiToSVG = require('ansi-to-svg');
 const emphasize = require('emphasize');
 const path = require('path');
+
 const options = {
   fontSize: 12,
   lineHeight: 25.6,
@@ -24,20 +25,26 @@ function highlightCode(code, lang = '') {
   return emphasize.highlightAuto(code);
 }
 
-function modifySVG(svg) {    
-  return svg.replace(/viewBox="([0-9.]+), ([0-9.]+), ([0-9.]+), ([0-9.]+)"/, (match, g1, g2, g3, g4) => {
-    return `height="${g4}" preserveAspectRatio="xMinYMax slice" viewBox="${g1}, ${g2}, 898, ${g4}"`;    
-  }).replace(/(<rect x="0" y="0" width=")([0-9.]+)(")/, (match, g1, g2, g3) => {
-    return [g1, 898, g3].join('');
-  });  
+function modifySVG(svg) {
+  return svg
+    .replace(
+      /viewBox="([0-9.]+), ([0-9.]+), ([0-9.]+), ([0-9.]+)"/,
+      (match, g1, g2, g3, g4) => `height="${g4}" preserveAspectRatio="xMinYMax slice" viewBox="${g1}, ${g2}, 898, ${g4}"`
+    )
+    .replace(/(<rect x="0" y="0" width=")([0-9.]+)(")/, (match, g1, g2, g3) => [g1, 898, g3].join(''));
 }
 
 function codeToSVG(code, range, { lang, theme = '3024.night' }) {
   const extractedCode = extract(code, range);
   const highlightedCode = highlightCode(extractedCode, lang);
-  return modifySVG(ansiToSVG(highlightedCode.value, Object.assign(options, {    
-    colors: path.resolve(__dirname, `../schemes/${theme}.itermcolors`)
-  })));  
+  return modifySVG(
+    ansiToSVG(
+      highlightedCode.value,
+      Object.assign(options, {
+        colors: path.resolve(__dirname, `../schemes/${theme}.itermcolors`)
+      })
+    )
+  );
 }
 
 module.exports = codeToSVG;
